f7f8ed7584ad4f7f69827bcd65be8843a31cd6a2
Raghav Kumar <jharaghav330@gmail.com>
2025-02-18 09:47:11 +0530

    create db connection in next js

---
+++ lib/db.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if(!MONGODB_URI) {
+    console.log("Mongodb uri is not availabe in env");
+};
+
+let cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+export async function connectionDb(){
+    if(cached.conn){
+        return cached.conn;
+    }
+
+    if(!cached.promise){
+        const opts = {
+            bufferCommands: true, // find now ----------------------
+            maxPoolSize: 10 // how many connection to create
+        }
+        
+        cached.promise = mongoose
+        .connect(MONGODB_URI, opts)
+        .then(() => mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null
+        throw error;
+    }
+
+    return cached.conn
+}
